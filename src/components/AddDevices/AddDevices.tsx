9c370b13529d57e8d208db5327fe49b75130c6ca
Andrew <ngoduong37na@gmail.com>
2024-04-29 16:31:41 +0700

    fix:form add device not working

--- src/components/AddDevices/AddDevices.tsx
+++ src/components/AddDevices/AddDevices.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { IonButton, IonContent, IonInput, IonText } from "@ionic/react";
 import { useForm } from "react-hook-form";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -18,12 +18,8 @@ import OutLetIcon from "../../assets/icons/outlet";
 import CircleExclamationIcon from "../../assets/icons/CircleExclamation";
 import classNames from "classnames";
 
-interface AddDeviceStepProps {
-  onSubmit: (data: any) => void;
-  errors: any;
-  register: any;
-  setIsError: (data: boolean) => void;
-  isError: boolean;
+interface AddDevicesSheetProps {
+  onClose: () => void;
 }
 const deviceData = {
   type: "temperature", //temperature || door || switch || light
@@ -35,144 +31,15 @@ const deviceData = {
   name_in_home: "temperature sensor",
   version: "1.1", //string
 };
-export const AddDeviceStep1: React.FC<AddDeviceStepProps> = ({
-  onSubmit,
-  errors,
-  register,
-  isError,
-  setIsError,
-}) => {
-  return (
-    <form className="add-devices-form ion-padding" onSubmit={onSubmit}>
-      <IonText className="add-devices-form-header">Add Device</IonText>
-      <IonText className="add-devices-form-sub-header">
-        Enter code of the device.
-      </IonText>
-      <IonInput
-        label="Device code"
-        color={errors.deviceCode ? "danger" : "light"}
-        labelPlacement="floating"
-        fill="outline"
-        type="text"
-        mode="md"
-        className="signup-form-item-input add-devices-form-input"
-        {...register("deviceCode", { required: true })}
-      />
-      <IonText className="add-devices-form-sub-header">
-        {errors.deviceCode && "Enter code of the device."}
-      </IonText>
 
-      <IonButton type="submit">Submit</IonButton>
-    </form>
-  );
-};
-
-interface AddDeviceStep3Props {
-  onSubmit: (data: any) => void;
-  errors: any;
-  register: any;
-}
-
-export const AddDeviceStep3: React.FC<AddDeviceStep3Props> = ({
-  onSubmit,
-  errors,
-  register,
-}) => {
-  return (
-    <form className="add-devices-form ion-padding" onSubmit={onSubmit}>
-      <IonText className="add-devices-form-header">Add Device</IonText>
-      <IonText className="add-devices-form-sub-header">
-        Enter Name of the device.
-      </IonText>
-      <IonInput
-        label="Rooms Name"
-        color={errors.deviceCode ? "danger" : "light"}
-        labelPlacement="floating"
-        fill="outline"
-        type="text"
-        mode="md"
-        className="signup-form-item-input add-devices-form-input"
-        {...register("deviceName", { required: true })}
-      />
-      <IonText className="add-devices-form-sub-header">
-        {errors.deviceCode && "Enter Devices name."}
-      </IonText>
-
-      <IonButton type="submit">Submit</IonButton>
-    </form>
-  );
-};
-interface AddDeviceStep4Props {
-  onSubmit: (data: any) => void;
-  errors: any;
-  register: any;
-}
-
-export const AddDeviceStep4: React.FC<AddDeviceStep3Props> = ({
-  onSubmit,
-  errors,
-  register,
-}) => {
-  return (
-    <form className="add-devices-form ion-padding" onSubmit={onSubmit}>
-      <IonText className="add-devices-form-header">Add Device</IonText>
-      <IonText className="add-devices-form-sub-header">
-        Enter Rooms of the device.
-      </IonText>
-      <IonInput
-        label="Rooms Name"
-        color={errors.deviceCode ? "danger" : "light"}
-        labelPlacement="floating"
-        fill="outline"
-        type="text"
-        mode="md"
-        className="signup-form-item-input add-devices-form-input"
-        {...register("RoomName", { required: true })}
-      />
-      <IonText className="add-devices-form-sub-header">
-        {errors.deviceCode && "Enter Rooms name."}
-      </IonText>
-
-      <IonButton type="submit">Submit</IonButton>
-    </form>
-  );
-};
-
-const AddDevicesSheet: React.FC<{ onClose: () => void }> = ({ onClose }) => {
-  const [isError, setIsError] = useState(false);
+const AddDevicesSheet: React.FC<AddDevicesSheetProps> = ({ onClose }) => {
+  const [step, setStep] = useState(0);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const swiperRef = React.useRef<any>(null); // Fix TypeScript error
-
-  const onSubmit = (data: any) => {
-    console.log(data.deviceCode);
-
-    console.log(data); // You can handle form submission here
-    handleOnSetSwiper(1);
-  };
-  const onSubmitStep3 = (data: any) => {
-    console.log(data.deviceCode);
-    handleOnSetSwiper(3);
-
-  };
-  const onSubmitStep4 = (data: any) => {
-    console.log(data.deviceCode);
-    handleOnSetSwiper(4);
-
-  };
-  const goToSetDeviceInformationStep = () => {
-    handleOnSetSwiper(2);
-  };
-
-  const handleOnSetSwiper = (number: number) => {
-    if (swiperRef.current) {
-      swiperRef.current.swiper.slideTo(number);
-    }
-  };
-
+  
   const ItemIcon = (type: string) => {
     switch (type) {
       case "temperature":
@@ -190,19 +57,53 @@ const AddDevicesSheet: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     }
   };
 
-  return (
-    <IonContent>
-      <Swiper ref={swiperRef}>
-        <SwiperSlide>
-          <AddDeviceStep1
-            isError={isError}
-            setIsError={setIsError}
-            onSubmit={handleSubmit(onSubmit)}
-            errors={errors}
-            register={register}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
+  const onSubmitStep = (data: any) => {
+    console.log(data);
+    // Handle form submission here
+    goToNextStep();
+  };
+
+  const goToNextStep = () => {
+    console.log(step)
+    setStep(step + 1);
+  };
+
+  useLayoutEffect(() => {
+    setStep(0);
+  }, []);
+
+  const renderFormStep = (step: number) => {
+    switch (step) {
+      case 0:
+        return (
+          <SwiperSlide key={step}>
+            <form className="add-devices-form ion-padding" onSubmit={handleSubmit(onSubmitStep)}>
+              <IonText className="add-devices-form-header">Add Device</IonText>
+              <IonText className="add-devices-form-sub-header">
+                Enter code of the device.
+              </IonText>
+              <IonInput
+                label="Device code"
+                color={errors.deviceCode ? "danger" : "light"}
+                labelPlacement="floating"
+                fill="outline"
+                type="text"
+                mode="md"
+                className="signup-form-item-input add-devices-form-input"
+                {...register("deviceCode", { required: true })}
+              />
+              <IonText className="add-devices-form-sub-header">
+                {errors.deviceCode && "Enter code of the device."}
+              </IonText>
+
+              <IonButton type="submit">Submit</IonButton>
+            </form>
+          </SwiperSlide>
+        );
+      case 1:
+        return (
+        <SwiperSlide key={step}>
+          <form className="add-devices-form ion-padding" onSubmit={handleSubmit(onSubmitStep)}>
           <div className="add-devices-form ion-padding">
             <IonText className="add-devices-form-header">Add Device</IonText>
             <div className="add-device-card ion-padding">
@@ -219,45 +120,89 @@ const AddDevicesSheet: React.FC<{ onClose: () => void }> = ({ onClose }) => {
                   />
                 </div>
               )}
-              <IonButton onClick={() => goToSetDeviceInformationStep()}>
+              <IonButton type="submit">
                 Add Devices
               </IonButton>
             </div>
           </div>
+          </form>
         </SwiperSlide>
-        <SwiperSlide>
-          <AddDeviceStep3
-            onSubmit={handleSubmit(onSubmitStep3)}
-            errors={errors}
-            register={register}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-        <AddDeviceStep3
-            onSubmit={handleSubmit(onSubmitStep4)}
-            errors={errors}
-            register={register}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="add-device-card ion-padding">
-            
-              {deviceData.type != "outlet" && (
-                <div className="add-device-card-icon-box">
-                  <FontAwesomeIcon
-                    className="add-device-card-icon add-device-card-icon-success"
-                    mode={'outline'}
-                    icon={faCircleCheck}
-                  />
-                </div>
-              )}
-              <IonText className="add-devices-form-header">Add Devices Success Fully!</IonText>
+        );
+        case 2:
+          return (
+            <SwiperSlide key={step}>
+              <form className="add-devices-form ion-padding" onSubmit={handleSubmit(onSubmitStep)}>
+                <IonText className="add-devices-form-header">Add Device</IonText>
+                <IonText className="add-devices-form-sub-header">
+                  Enter Name of the device.
+                </IonText>
+                <IonInput
+                  label="Device Name"
+                  color={errors.deviceCode ? "danger" : "light"}
+                  labelPlacement="floating"
+                  fill="outline"
+                  type="text"
+                  mode="md"
+                  className="signup-form-item-input add-devices-form-input"
+                  {...register("deviceName", { required: true })}
+                />
+                <IonText className="add-devices-form-sub-header">
+                  {errors.deviceCode && "Enter code of the device."}
+                </IonText>
+  
+                <IonButton type="submit">Submit</IonButton>
+              </form>
+            </SwiperSlide>
+          );
+      case 3:
+        return (
+          <SwiperSlide key={step}>
+            <form className="add-devices-form ion-padding" onSubmit={handleSubmit(onSubmitStep)}>
+              <IonText className="add-devices-form-header">Add Device</IonText>
+              <IonText className="add-devices-form-sub-header">
+                Enter Rooms of the device.
+              </IonText>
+              <IonInput
+                label="Rooms Name"
+                color={errors.deviceCode ? "danger" : "light"}
+                labelPlacement="floating"
+                fill="outline"
+                type="text"
+                mode="md"
+                className="signup-form-item-input add-devices-form-input"
+                {...register("RoomName", { required: true })}
+              />
+              <IonText className="add-devices-form-sub-header">
+                {errors.deviceCode && "Enter Rooms name."}
+              </IonText>
 
-              <IonButton onClick={() => onClose()}>
-                Done
-              </IonButton>
+              <IonButton type="submit">Submit</IonButton>
+            </form>
+          </SwiperSlide>
+        );
+      default:
+        return (
+          <SwiperSlide key={step}>
+            <div className="add-device-card ion-padding">
+              <div className="add-device-card-icon-box">
+                <FontAwesomeIcon
+                  className="add-device-card-icon add-device-card-icon-success"
+                  mode={"outline"}
+                  icon={faCircleCheck}
+                />
+              </div>
+              <IonText className="add-devices-form-header">Add Devices Success Fully!</IonText>
+              <IonButton onClick={onClose}>Done</IonButton>
             </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        );
+    }
+  };
+
+  return (
+    <IonContent>
+      <Swiper noSwiping>
+        {renderFormStep(step)}
       </Swiper>
     </IonContent>
   );
